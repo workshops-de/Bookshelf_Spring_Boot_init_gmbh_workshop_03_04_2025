25216cc9261291c795543eda974e1e090d0358f9
Birgit Kratz <mail@birgitkratz.de>
2025-04-08 18:01:46 +0200

    Day 4: TestConfiguration

--- src/test/java/de/workshops/bookshelf/book/BookRestControllerMockMvcTest.java
+++ src/test/java/de/workshops/bookshelf/book/BookRestControllerMockMvcTest.java
@@ -2,10 +2,12 @@ package de.workshops.bookshelf.book;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import de.workshops.bookshelf.ObjectMapperConfiguration;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
@@ -14,11 +16,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest()
 @AutoConfigureMockMvc
+@Import(ObjectMapperConfiguration.class)
 class BookRestControllerMockMvcTest {
 
     @Autowired
@@ -30,6 +34,7 @@ class BookRestControllerMockMvcTest {
     @Test
     void getAllBooks() throws Exception {
         mockMvc.perform(get("/book"))
+                .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$").isArray())
                 .andExpect(jsonPath("$", hasSize(3)));
@@ -38,6 +43,7 @@ class BookRestControllerMockMvcTest {
     @Test
     void getAllBooksWithPayload() throws Exception {
         var mvcResult = mockMvc.perform(get("/book"))
+                .andDo(print())
                 .andExpect(status().isOk())
                 .andReturn();
 
@@ -59,6 +65,5 @@ class BookRestControllerMockMvcTest {
                                   }
                                 """))
                 .andExpect(status().isOk());
-
     }
 }
